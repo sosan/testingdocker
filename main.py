ad05398a67a8b43b4215fe1bb962f15e3699982b
jose <josloe@gmail.com>
2020-01-13 20:26:59 +0100

    init docker

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import render_template
+app = Flask(__name__)
+app.secret_key = "holal"
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", 7050, debug=True)
+
